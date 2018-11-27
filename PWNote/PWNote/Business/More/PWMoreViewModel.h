3ef6544f8064fa7007facf327a49438c954747dd
WenMingYan <wenmingyan1990@gmail.com>
2018-11-27 11:42:26 +0800

    update

---
+++ PWNote/PWNote/Business/More/PWMoreViewModel.h
@@ -0,0 +1,19 @@
+//
+//  PWMoreViewModel.h
+//  PWNote
+//
+//  Created by 明妍 on 2018/11/26.
+//  Copyright © 2018 明妍. All rights reserved.
+//
+
+#import "PWViewModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PWMoreViewModel : PWViewModel
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
