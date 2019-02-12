9da0afba069760b1fe27d20725c5605d40a60459
WenMingYan <wenmingyan1990@gmail.com>
2019-02-12 09:30:23 +0800

    Revert "溢出Pods"
    This reverts commit e6a7c579a268afcf65a2f101834e302fb0fb143b.

---
+++ PWNote/PWNote/Business/Category/View/PWCategoryFooterItemView.m
@@ -0,0 +1,91 @@
+//
+//  PWCategoryFooterItemView.m
+//  PWNote
+//
+//  Created by mingyan on 2019/1/8.
+//  Copyright © 2019 明妍. All rights reserved.
+//
+
+#import "PWCategoryFooterItemView.h"
+#import "PWCagegoryFooterViewModel.h"
+
+@interface PWCategoryFooterItemView ()
+
+@property (nonatomic, strong) UILabel *iconLabel; /**< 添加icon  */
+@property (nonatomic, strong) UILabel *titleLabel; /**< 添加清单  */
+
+@end
+
+@implementation PWCategoryFooterItemView
+
+#pragma mark - --------------------dealloc ------------------
+#pragma mark - --------------------life cycle--------------------
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self initView];
+    }
+    return self;
+}
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self initView];
+    }
+    return self;
+}
+
+- (void)initView {
+    [self addSubview:self.iconLabel];
+    [self addSubview:self.titleLabel];
+    
+    [self.iconLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.height.mas_equalTo(24);
+        make.centerY.equalTo(self).mas_offset(-kSafeArea_Bottom / 2);
+        make.left.mas_equalTo(kMargin);
+    }];
+    
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.iconLabel.mas_centerY);
+        make.left.mas_equalTo(self.iconLabel.mas_right).mas_offset(8);
+    }];
+    
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self initView];
+}
+
+- (void)setViewModel:(id<PWViewModelProtocol>)viewModel {
+    [super setViewModel:viewModel];
+    if ([viewModel isKindOfClass:[PWCagegoryFooterViewModel class]]) {
+        PWCagegoryFooterViewModel *model = (PWCagegoryFooterViewModel *)viewModel;
+        self.titleLabel.text = model.title;
+    }
+}
+
+#pragma mark - --------------------UITableViewDelegate--------------
+#pragma mark - --------------------CustomDelegate--------------
+#pragma mark - --------------------Event Response--------------
+#pragma mark - --------------------private methods--------------
+#pragma mark - --------------------getters & setters & init members ------------------
+
+- (UILabel *)iconLabel {
+    if (!_iconLabel) {
+        _iconLabel = [[UILabel alloc] init];
+        _iconLabel.font = [AMIconfont fontWithSize:24];
+        _iconLabel.text = XIconAdd;
+        _iconLabel.textColor = kThemeColor;
+    }
+    return _iconLabel;
+}
+
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] init];
+        _titleLabel.textColor = kThemeColor;
+    }
+    return _titleLabel;
+}
+
+@end
---
+++ PWNote/PWNote/Test/PWTestHomeViewController.m
@@ -0,0 +1,92 @@
+//
+//  PWTestHomeViewController.m
+//  PWNote
+//
+//  Created by 明妍 on 2018/11/25.
+//  Copyright © 2018 明妍. All rights reserved.
+//
+
+#import "PWTestHomeViewController.h"
+
+@interface PWTestHomeViewController () <UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation PWTestHomeViewController
+
+__PW_ROUTER_REGISTER__
+
+#pragma mark - --------------------dealloc ------------------
+#pragma mark - --------------------life cycle--------------------
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self initData];
+    [self initView];
+}
+
+- (void)initData {
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+}
+
+- (void)initView {
+    self.title = @"test";
+    [self.view addSubview:self.tableView];
+    self.tableView.frame = self.view.bounds;
+}
+#pragma mark - --------------------UITableViewDelegate--------------
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    cell.textLabel.text = [[self names] objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.names.count;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *url = [[self names] objectAtIndex:indexPath.row];
+    [self routerURL:url withParam:nil];
+}
+
+#pragma mark - --------------------CustomDelegate--------------
+#pragma mark - --------------------Event Response--------------
+
++ (NSString *)urlName {
+    return @"test";
+}
+
+#pragma mark - --------------------private methods--------------
+#pragma mark - --------------------getters & setters & init members ------------------
+
+- (NSArray *)names {
+    return @[
+             @"pwnote://setting",
+             @"pwnote://more",
+             @"pwnote://home",
+             @"pwnote://search",
+             ];
+}
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
+
+@end
